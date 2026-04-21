c1cf7a19747169f32b89123063f7a3888e28a134
zhikanyeye <ai2845612055@outlook.com>
2026-04-22 00:45:34 +0800

    Fix navigation tracker prerender

--- components/NavigationTracker.tsx
+++ components/NavigationTracker.tsx
@@ -1,20 +1,17 @@
 'use client';
 
-import { useEffect, useMemo } from 'react';
-import { usePathname, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+import { usePathname } from 'next/navigation';
 
 const CURRENT_ROUTE_KEY = 'kvideo-nav-current';
 const PREVIOUS_ROUTE_KEY = 'kvideo-nav-previous';
 
 export function NavigationTracker() {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
-  const route = useMemo(() => {
-    const query = searchParams.toString();
-    return query ? `${pathname}?${query}` : pathname;
-  }, [pathname, searchParams]);
 
   useEffect(() => {
+    const query = window.location.search;
+    const route = query ? `${pathname}${query}` : pathname;
     const currentRoute = sessionStorage.getItem(CURRENT_ROUTE_KEY);
 
     if (currentRoute && currentRoute !== route) {
@@ -22,7 +19,7 @@ export function NavigationTracker() {
     }
 
     sessionStorage.setItem(CURRENT_ROUTE_KEY, route);
-  }, [route]);
+  }, [pathname]);
 
   return null;
 }
